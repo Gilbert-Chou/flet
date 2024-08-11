190c09e196a6337c71d4ee5aa330ae2779f4bf2d
shihhochou <shihhochou@synology.com>
2024-08-12 01:52:46 +0800

    fix: fix deprecated code and remove vm_mode_switch_btn

--- lazy_typewriter/lazy_typewriter.py
+++ lazy_typewriter/lazy_typewriter.py
@@ -9,7 +9,7 @@ from pynput.keyboard import Controller as KeyboardController
 from pynput.keyboard import Key as KeyboardKey
 
     
-class SmallPopupMenuItem(ft.PopupMenuItem):
+class SettingPopupMenuItem(ft.PopupMenuItem):
 
     def __init__(self, page: ft.Page, text: str, config_name: str, customized_on_click=None):
         check_icon = ft.Icon(ft.icons.CHECK if getattr(sys_config, config_name) else "", size=20) 
@@ -29,18 +29,26 @@ class SmallPopupMenuItem(ft.PopupMenuItem):
 
         return super().__init__(content=container, on_click=on_click, height=1, checked=None)
 
-class TypeText(ft.UserControl):
+class TypeText(ft.Row):
 
-    def __init__(self, lv: ft.ListView, vm_mode_switch_btn: ft.Switch, trigger_snack_bar, type_str_field_value=""):
+    def __init__(self, lv: ft.ListView, trigger_snack_bar, type_str_field_value=""):
         super().__init__()
         self.lv = lv
         self.trigger_snack_bar = trigger_snack_bar
-        self.vm_mode_switch_btn = vm_mode_switch_btn
         self.type_str_field = ft.TextField(value=type_str_field_value, autofocus=True, cursor_color=ft.colors.BLACK,
                                            color=ft.colors.BLACK, text_align=ft.TextAlign.LEFT, expand=True, width=300, bgcolor=ft.colors.GREY_400)
         self.drag_icon = ft.Icon(ft.icons.DRAG_INDICATOR, scale=1.80)
         self.pynput_keyboard = KeyboardController()
-        self.build()
+
+        self.controls = [
+            ft.Draggable(
+                content=self.drag_icon,
+                content_when_dragging=ft.Icon(ft.icons.DRAG_INDICATOR, scale=1.8, color=ft.colors.PRIMARY),
+            ),
+            self.type_str_field,
+            ft.IconButton(ft.icons.KEYBOARD_RETURN, on_click=self.keyboard_type),
+            ft.IconButton(ft.icons.DELETE, on_click=self.delete_text_field)
+        ]
 
     def delete_text_field(self, e):
         self.lv.controls.remove(self)
@@ -54,41 +62,22 @@ class TypeText(ft.UserControl):
         elif platform.system() == 'Darwin':
             keyboard.press_and_release('command+tab')
         time.sleep(0.3)
-        if self.vm_mode_switch_btn.value:
+        if getattr(sys_config, "vm_mode"):
             self._type_text_with_pynput(self.type_str_field.value)
         else:
             keyboard.write(self.type_str_field.value, delay=0.01)
         self.update()
         self.trigger_snack_bar(e, "Typed")
 
-    def build(self):
-        return ft.DragTarget(
-            on_accept=self._drag_accept,
-            content=ft.Row(
-                [
-                    ft.Draggable(
-                        content=self.drag_icon,
-                        content_when_dragging=ft.Icon(ft.icons.DRAG_INDICATOR, scale=1.8, color=ft.colors.PRIMARY),
-                    ),
-                    self.type_str_field,
-                    ft.IconButton(ft.icons.KEYBOARD_RETURN,
-                                  on_click=self.keyboard_type),
-                    ft.IconButton(ft.icons.DELETE,
-                                  on_click=self.delete_text_field)
-                ],
-                alignment=ft.MainAxisAlignment.CENTER,
-            ),
-        )
-
-    def _drag_accept(self, e: ft.DragTargetAcceptEvent):
+    def drag_accept(self, e: ft.DragTargetAcceptEvent):
         src = self.page.get_control(e.src_id)
         srcouce_index = None
         target_index = None
 
         for i, line in enumerate(self.lv.controls):
-            if line.drag_icon == src.content:
+            if line.content.content.drag_icon == src.content:
                 srcouce_index = i
-            if line.drag_icon == self.drag_icon:
+            if line.content.content.drag_icon == self.drag_icon:
                 target_index = i
 
         if srcouce_index is None or target_index is None:
@@ -99,14 +88,14 @@ class TypeText(ft.UserControl):
             return
 
         for i, line in enumerate(self.lv.controls):
-            if line.drag_icon == src.content:
+            if line.content.content.drag_icon == src.content:
                 srcouce = self.lv.controls.pop(i)
                 break
 
         self.lv.update()
 
         for i, line in enumerate(self.lv.controls):
-            if line.drag_icon == self.drag_icon:
+            if line.content.content.drag_icon == self.drag_icon:
                 if srcouce_index > target_index:
                     self.lv.controls.insert(i, srcouce)
                 else:
@@ -139,12 +128,11 @@ class TypeText(ft.UserControl):
 
 class TypeTextListView(ft.ListView):
 
-    def __init__(self, vm_mode_switch_btn: ft.Switch, trigger_snack_bar, save_path=""):
+    def __init__(self: ft.Switch, trigger_snack_bar, save_path=""):
         super().__init__(expand=1)
         self.trigger_snack_bar = trigger_snack_bar
         self.save_path = self.get_save_path() if save_path == "" else save_path
-        self.lv = ft.ListView(expand=1, spacing=10, padding=20)
-        self.vm_mode_switch_btn = vm_mode_switch_btn
+        self.controls = []
 
     def get_save_path(self):
         if platform.system() == 'Windows':
@@ -156,14 +144,14 @@ class TypeTextListView(ft.ListView):
         if os.path.exists(self.save_path):
             with open(self.save_path, "r", encoding='utf-8') as f:
                 for line in f.readlines():
-                    self.lv.controls.append(TypeText(
-                        self.lv, self.vm_mode_switch_btn, self.trigger_snack_bar, type_str_field_value=line.strip()))
-        if self.lv.controls == []:
-            self.lv.controls.append(TypeText(self.lv, self.vm_mode_switch_btn, self.trigger_snack_bar))
+                    self.controls.append(self._new_drag_target(TypeText(
+                        self, self.trigger_snack_bar, type_str_field_value=line.strip())))
+        if self.controls == []:
+            self.controls.append(self._new_drag_target(TypeText(self, self.trigger_snack_bar)))
         self.update()
 
     def add_typing_text_field(self, e):
-        self.lv.controls.insert(0, TypeText(self.lv, self.vm_mode_switch_btn, self.trigger_snack_bar))
+        self.controls.insert(0, self._new_drag_target(TypeText(self, self.trigger_snack_bar)))
         self.update()
 
     def save_content(self, e):
@@ -171,14 +159,21 @@ class TypeTextListView(ft.ListView):
             os.makedirs(os.path.dirname(self.save_path))
 
         with open(self.save_path, "w", encoding='utf-8') as f:
-            for control in self.lv.controls:
+            for control in self.controls:
                 if isinstance(control, TypeText) and control.type_str_field.value != "":
                     f.write(control.type_str_field.value + "\n")
         
         self.trigger_snack_bar(e, "Saved")
+    
+    def _new_drag_target(self, content: TypeText):
+        return ft.Container(
+            ft.DragTarget(
+                on_accept=content.drag_accept,
+                content=content,
+            ),
+            padding=ft.padding.symmetric(vertical=3)
+        )
 
-    def build(self):
-        return self.lv
 
 def main(page: ft.Page):
     page.title = "Lazy typewriter"
@@ -189,10 +184,7 @@ def main(page: ft.Page):
     page.window.always_on_top = True
     page.bgcolor = ft.colors.GREY_800
     page.theme_mode = ft.ThemeMode.DARK
-
-    vm_mode_switch_btn = ft.Switch(
-        label="VM mode", label_position=ft.LabelPosition.LEFT, value=False, scale=0.9)
-        
+       
     def trigger_snack_bar(e, text):
         page.snack_bar = ft.SnackBar(
             content=ft.Text(text, color=ft.colors.BLACK, weight=ft.FontWeight.BOLD),
@@ -203,7 +195,7 @@ def main(page: ft.Page):
         page.snack_bar.open = True
         page.update()
 
-    type_text_list_view = TypeTextListView(vm_mode_switch_btn, trigger_snack_bar)
+    type_text_list_view = TypeTextListView(trigger_snack_bar)
 
     def pin_window(e):
         page.window.always_on_top = not page.window.always_on_top
@@ -225,11 +217,11 @@ def main(page: ft.Page):
             ft.IconButton(ft.icons.SAVE, on_click=save_content),
             ft.PopupMenuButton(
                 items=[
-                    SmallPopupMenuItem(page=page, text="VM mode", config_name="vm_mode"),
+                    SettingPopupMenuItem(page=page, text="VM mode", config_name="vm_mode"),
                     ft.PopupMenuItem(),  # divider
-                    SmallPopupMenuItem(page=page, text="Pin", config_name="pin", customized_on_click=pin_window),
+                    SettingPopupMenuItem(page=page, text="Pin", config_name="pin", customized_on_click=pin_window),
                     ft.PopupMenuItem(),  # divider
-                    SmallPopupMenuItem(page=page, text="Slow mode", config_name="slow_mode"),
+                    SettingPopupMenuItem(page=page, text="Slow mode", config_name="slow_mode"),
                 ]
             ),
         ],
