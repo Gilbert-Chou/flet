9f93cb6622dcd23fd41f29cadd7e5b52820e304b
shihhochou <shihhochou@synology.com>
2024-08-20 00:59:39 +0800

    refact: refactor TypeTextExpansionPanel

--- lazy_typewriter/lazy_typewriter.py
+++ lazy_typewriter/lazy_typewriter.py
@@ -33,9 +33,8 @@ class SettingPopupMenuItem(ft.PopupMenuItem):
 
 class TypeText(ft.Row):
 
-    def __init__(self, lv: ft.ListView, type_str_field_value=""):
+    def __init__(self, type_str_field_value=""):
         super().__init__()
-        self.lv = lv
         self.type_str_field = ft.TextField(value=type_str_field_value, cursor_color=ft.colors.BLACK,
                                            color=ft.colors.BLACK, text_align=ft.TextAlign.LEFT, expand=True, width=300, bgcolor=ft.colors.GREY_400)
         self.drag_icon = ft.Icon(ft.icons.DRAG_INDICATOR, scale=1.80)
@@ -50,13 +49,17 @@ class TypeText(ft.Row):
             ft.IconButton(ft.icons.KEYBOARD_RETURN, on_click=self.keyboard_type),
             ft.IconButton(ft.icons.DELETE, on_click=self.delete_text_field)
         ]
+    
+    @property
+    def text_value(self):
+        return self.type_str_field.value
 
     def delete_text_field(self, e):
-        for i, line in enumerate(self.lv.controls):
-            if self._drag_icon_path(line) == self.drag_icon:
-                self.lv.controls.pop(i)
+        for i, line in enumerate(self.get_list_view().controls):
+            if self._check_typetext_container_instance(line) and line.get_drag_icon() == self.drag_icon:
+                self.get_list_view().controls.pop(i)
                 break
-        self.lv.update()
+        self.get_list_view().update()
 
     def keyboard_type(self, e):
         if self.type_str_field.value == "":
@@ -73,43 +76,52 @@ class TypeText(ft.Row):
         self.update()
         trigger_snack_bar(self.parent, "Typed")
 
+    def get_list_view(self) -> ft.Control:
+
+        def find_parent(control: ft.Control):
+            if isinstance(control, TypeTextExpansionPanelList):
+                return control
+            return find_parent(control.parent)
+
+        return find_parent(self.parent)
+
     def drag_accept(self, e: ft.DragTargetAcceptEvent):
         src = self.page.get_control(e.src_id)
         srcouce_index = None
         target_index = None
 
-        for i, line in enumerate(self.lv.controls):
-            if self._drag_icon_path(line) == src.content:
+        for i, line in enumerate(self.get_list_view().controls):
+            if self._check_typetext_container_instance(line) and line.get_drag_icon() == src.content:
                 srcouce_index = i
-            if self._drag_icon_path(line) == self.drag_icon:
+            if self._check_typetext_container_instance(line) and line.get_drag_icon() == self.drag_icon:
                 target_index = i
 
         if srcouce_index is None or target_index is None:
             raise Exception("Can't find srcouce_index or target_index")
 
         if srcouce_index == target_index:
-            self.lv.update()
+            self.get_list_view().update()
             return
 
-        for i, line in enumerate(self.lv.controls):
-            if self._drag_icon_path(line) == src.content:
-                srcouce = self.lv.controls.pop(i)
+        for i, line in enumerate(self.get_list_view().controls):
+            if self._check_typetext_container_instance(line) and line.get_drag_icon() == src.content:
+                srcouce = self.get_list_view().controls.pop(i)
                 break
 
-        self.lv.update()
+        self.get_list_view().update()
 
-        for i, line in enumerate(self.lv.controls):
-            if self._drag_icon_path(line) == self.drag_icon:
+        for i, line in enumerate(self.get_list_view().controls):
+            if self._check_typetext_container_instance(line) and line.get_drag_icon() == self.drag_icon:
                 if srcouce_index > target_index:
-                    self.lv.controls.insert(i, srcouce)
+                    self.get_list_view().controls.insert(i, srcouce)
                 else:
-                    self.lv.controls.insert(i+1, srcouce)
+                    self.get_list_view().controls.insert(i+1, srcouce)
                 break
 
-        self.lv.update()
+        self.get_list_view().update()
     
-    def _drag_icon_path(self, line: ft.Control):
-        return line.header.title.content.content.drag_icon
+    def _check_typetext_container_instance(self, line: ft.Control):
+        return isinstance(line, TypeTextExpansionPanel)
 
     def _type_text_with_pynput(self, type_str: str):
         need_convert_char = "~!@#$%^&*()_+{}|:\"<>?ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -140,43 +152,15 @@ class TypeText(ft.Row):
             time.sleep(hard_sleep_time if hard_sleep_time else getattr(sys_config, "slow_mode_time"))
 
 
-class TypeTextExpansionPanelList(ft.ExpansionPanelList):
+class TypeTextExpansionPanel(ft.ExpansionPanel):
 
-    def __init__(self):
-        super().__init__(expand=1)
-        self.controls = []
-        self.expand_icon_color=ft.colors.AMBER
-        self.divider_color=ft.colors.AMBER
-
-    def add_typing_text_field(self, e):
-        self.controls.insert(0, self._drag_target_factory())
-        self.update()
-
-    def restore_saved_content(self):
-        for line in data_config.load_type_content():
-            self.controls.append(self._drag_target_factory(line['content'], line['note']))
-        if self.controls == []:
-            self.controls.append(self._drag_target_factory())
-        self.update()
-
-    def save_content(self, e):
-        content = []
-        for control in self.controls:
-            if isinstance(control, ft.ExpansionPanel):
-                type_content = control.header.title.content.content.type_str_field.value
-                if type_content != "":
-                    note = control.content.title.value if control.content.title.value else ""
-                    content.append({"content": type_content, "note": note})
-
-        data_config.save_type_content(content)
-        trigger_snack_bar(self.parent, "Saved")
-
-    def _drag_target_factory(self, text: str = "", note: str = ""):
-        type_text = TypeText(self, type_str_field_value=text.strip())
+    def __init__(self, text: str = "", note: str = ""):
+        super().__init__()
+        self.type_text = TypeText(type_str_field_value=text.strip())
         new_content = ft.Container(
             ft.DragTarget(
-                on_accept=type_text.drag_accept,
-                content=type_text,
+                on_accept=self.type_text.drag_accept,
+                content=self.type_text,
             ),
             padding=ft.padding.symmetric(vertical=3)
         )
@@ -186,19 +170,68 @@ class TypeTextExpansionPanelList(ft.ExpansionPanelList):
             ft.colors.RED_800,
             ft.colors.BLACK45,
         ]
-        exp = ft.ExpansionPanel(
-            bgcolor=colors[3],
-            header=ft.ListTile(title=new_content),
-        )
+        self.bgcolor = colors[3]
+        self.header = ft.ListTile(title=new_content)
+        self.note = ft.Text(note.strip())
 
-        exp.content = ft.ListTile(
-            title=ft.Text(note.strip()),
+        self.content = ft.ListTile(
+            title=self.note,
             content_padding=ft.padding.only(left=50, right=10),
-            trailing=ft.IconButton(ft.icons.MODE_OUTLINED, data=exp),
+            trailing=ft.IconButton(ft.icons.MODE_OUTLINED, data=self),
         )
 
-        return exp
-         
+    @property
+    def text_value(self) -> str:
+        return self.type_text.text_value
+    
+    @property
+    def note_value(self) -> str:
+        return self.note.value
+
+    def get_list_view(self) -> ft.ExpansionPanelList:
+        
+        def find_parent(control: ft.Control):
+            if isinstance(control, TypeTextExpansionPanelList):
+                return control
+            return find_parent(control.parent)
+
+        return find_parent(self)
+
+    def get_drag_icon(self):
+        return self.type_text.drag_icon
+
+
+class TypeTextExpansionPanelList(ft.ExpansionPanelList):
+
+    def __init__(self):
+        super().__init__()
+        self.controls = []
+        self.expand_icon_color = ft.colors.AMBER
+        self.divider_color = ft.colors.AMBER
+
+    def add_typing_text_field(self, e):
+        self.controls.insert(0, TypeTextExpansionPanel())
+        self.update()
+
+    def restore_saved_content(self):
+        for line in data_config.load_type_content():
+            self.controls.append(TypeTextExpansionPanel(line['content'], line['note']))
+        if self.controls == []:
+            self.controls.append(TypeTextExpansionPanel())
+        self.update()
+
+    def save_content(self, e):
+        content = []
+        for control in self.controls:
+            if isinstance(control, TypeTextExpansionPanel):
+                type_content = control.text_value
+                if type_content != "":
+                    note = control.note_value if control.note_value else ""
+                    content.append({"content": type_content, "note": note})
+
+        data_config.save_type_content(content)
+        trigger_snack_bar(self.parent, "Saved")
+
 
 def main(page: ft.Page):
     page.title = "Lazy typewriter"
@@ -206,6 +239,7 @@ def main(page: ft.Page):
     page.window.width = 500
     page.window.min_height = 470
     page.window.min_width = 500
+    page.window.always_on_top = getattr(sys_config, "pin")
     page.bgcolor = ft.colors.GREY_800
     page.theme_mode = ft.ThemeMode.DARK
 
